fcb923234a24c881a7c2967b49e253f4a6d6458f
Philip Wang <powdercheeze@gmail.com>
2022-04-26 15:06:55 -0400

    Update files

--- final-project/piet-interpreter/src/ImageLoader.hs
+++ final-project/piet-interpreter/src/ImageLoader.hs
@@ -1,4 +1,15 @@
 module ImageLoader where
-    
+
 import System.IO (openFile, IOMode(ReadMode, WriteMode, ReadWriteMode))
-import Graphics.Image.IO.Formats
+import Codec.Picture
+
+
+path :: String
+path = "../images/hw.gif"
+
+readPixels = do
+    image <- readImage path
+    return convertRGBA8 <$> image
+
+
+
