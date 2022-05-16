863451e3a034fe7654bfc553e4bd7dd0d82162e1
Philip Wang <powdercheeze@gmail.com>
2022-05-15 22:54:35 -0400

    Fix OBO

--- final-project/piet-interpreter/src/Example.hs
+++ final-project/piet-interpreter/src/Example.hs
@@ -6,7 +6,7 @@ import ImageLoader
 import Codec.Picture
 import Data.Vector ((!), (!?))
 path :: String
-path = "/home/philip2000/Documents/CMSC488B/final-project/piet-interpreter/images/euclid1.png"
+path = "/home/philip2000/Documents/CMSC488B/final-project/piet-interpreter/images/hw1-small.gif"
 
 example :: IO (Either String PietProgram)
 example = do
